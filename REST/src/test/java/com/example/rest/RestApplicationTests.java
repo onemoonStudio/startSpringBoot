0265681585e9ff3c3bd05bded059c76f4252ad25
onemoon <onemoonstudio@gmail.com>
2018-07-02 23:09:14 +0900

    Add : rest build test
    rest build with jenkins and docker

--- REST/src/test/java/com/example/rest/RestApplicationTests.java
+++ REST/src/test/java/com/example/rest/RestApplicationTests.java
@@ -1,4 +1,4 @@
-package com.onemoon.springweb;
+package com.example.rest;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ApplicationTests {
+public class RestApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
