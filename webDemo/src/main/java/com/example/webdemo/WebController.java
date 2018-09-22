64f57e06308af561e554c832e654ff95cb9f1a21
onemoon <onemoonstudio@gmail.com>
2018-09-22 17:02:26 +0900

    Feat : web source
    add handlebars , library and simple handlebars logic

--- webDemo/src/main/java/com/example/webdemo/WebController.java
+++ webDemo/src/main/java/com/example/webdemo/WebController.java
@@ -1,19 +1,27 @@
 package com.example.webdemo;
 
-
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.io.IOException;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @AllArgsConstructor
 public class WebController {
+
     @GetMapping(value = "/")
     public String main(){
+
         return "main";
     }
+
+    @GetMapping(value = "/this")
+    public ModelAndView testThis(){
+        ModelAndView mv = new ModelAndView("this");
+        mv.addObject("body","wow");
+        return mv;
+    }
+
 //    @GetMapping(value = "/hbtest")
 //    public String hbtest() throws IOException {
 //        Handlebars handlebars = new Handlebars();
